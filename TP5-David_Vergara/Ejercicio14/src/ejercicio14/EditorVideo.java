c75cf448dff81befb8f51ef01507582d3e2077df
David Vergara <david.e.vergara2025@gmail.com>
2025-09-25 12:02:38 -0300

    Ejercicios completos

--- TP5-David_Vergara/Ejercicio14/src/ejercicio14/EditorVideo.java
+++ TP5-David_Vergara/Ejercicio14/src/ejercicio14/EditorVideo.java
@@ -9,17 +9,9 @@ package ejercicio14;
  * @author david
  */
 public class EditorVideo {
-    /*EditorVideo - Proyecto - Render
-a. Asociación unidireccional: Render → Proyecto
-b. Dependencia de creación: EditorVideo.exportar(String, Proyecto)
-c. Clases y atributos:
-i. Render: formato.
-ii. Proyecto: nombre, duracionMin.
-iii. EditorVideo->método: void exportar(String formato, Proyecto proyecto) */
     public void exportar(String formato, Proyecto proyecto) {
         Render render = new Render(formato);
         render.setProyecto(proyecto);
-        // Lógica para exportar el proyecto en el formato especificado
         System.out.println("\nExportando el proyecto " + proyecto.getNombre() + " en formato " + render.getFormato());
     }
 }
